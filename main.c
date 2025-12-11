6a9d90710f107aa11e6819d0b893b876871b132c
Andre Ugolini <andreugolini@unochapeco.edu.br>
2025-12-11 17:52:04 -0300

    Adicionar função de push para inserir novo node ao final da lista.

--- main.c
+++ main.c
@@ -13,3 +13,20 @@ Node *createNode(int data) {
   newNode->prev = NULL;
   return newNode;
 }
+
+// Função que adiciona um novo node ao final da lista
+void push(Node **head, int data) {
+  Node *newNode = createNode(data);
+
+  if (*head == NULL) {
+    *head = newNode;
+    return;
+  }
+
+  Node *temp = *head;
+  while (temp->next != NULL) {
+    temp = temp->next;
+  }
+  temp->next = newNode;
+  newNode->prev = temp;
+}
