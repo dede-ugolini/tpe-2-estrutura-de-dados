90e74e291982c6934d22eaf1d7530ca643e0abe0
Andre Ugolini <andreugolini@unochapeco.edu.br>
2026-02-10 16:08:50 -0300

    Header responsável pelo auto complete.

---
+++ src/auto_complete.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include <readline/history.h>
+#include <readline/readline.h>
+
+typedef struct {
+  char *name;
+  rl_icpfunc_t *func;
+  char *doc;
+} Command;
+
+char *dupstr(char *s);
+
+char *command_generator(const char *text, int state);
+
+char **fileman_completion(const char *text, int start, int end);
+
+void initialize_readline(char *progname);
+
+int execute_line(char *line);
