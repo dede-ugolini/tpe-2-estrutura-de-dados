bf91d81f6ba486341f7894e3a28d80933c717a2e
Andre Ugolini <andreugolini@unochapeco.edu.br>
2026-02-10 23:11:16 -0300

    Remover funções que não vão ser mais úteis devido a nova implementação da lib readline.

--- src/main.c
+++ src/main.c
@@ -17,13 +17,6 @@ typedef struct Node {
   struct Node *prev;
 } Node;
 
-typedef void (*command_fn)(Node **head);
-
-/* typedef struct {
-  const char *name;
-  command_fn fn;
-} Command; */
-
 Node *createNode(int data) {
   Node *newNode = (Node *)malloc(sizeof(Node));
   newNode->data = data;
@@ -259,157 +252,27 @@ void fill(Node *head) {
     push(&head, rand() % 100);
   }
 }
-void menu(Node *head) {
-
-  int option = 0;
-  do {
-    printf("============ Hello World ===============\n");
-    printf("1 - pop (remover o último node da lista.)\n");
-    printf("2 - push (adicionar um node ao final da lista.)\n");
-    printf("3 - shift (remover primeiro node da lista.)\n");
-    printf("4 - unShift (adicionar um node ao início da lista.)\n");
-    printf("5 - inserir valor em determinada posição\n");
-    printf("6 - excluir valor em determinada posição\n");
-    printf("7 - printar lista de frente para trás\n");
-    printf("8 - printar lista de trás para frente\n");
-    printf("9 - ordenar a lista em ordem crescente\n");
-    printf("10 - ordenar a lista em ordem decrescente\n");
-    printf("11 - preencher a lista com numeros aleatorios\n");
-    printf("0 - Sair");
-
-    int data = 0;
-    int position = 0;
-
-    printf("\nEscolha uma opção.\n");
-    scanf("%d", &option);
-
-    switch (option) {
-    case 1:
-      pop(&head);
-      break;
-    case 2:
-      printf("Digite o valor que o node vai armazenar: ");
-      scanf("%d", &data);
-      push(&head, data);
-      break;
-    case 3:
-      shift(&head);
-      break;
-    case 4:
-      printf("Digite o valor que o node vai armazenar: ");
-      scanf("%d", &data);
-      unShift(&head, data);
-      break;
-    case 5:
-      printf("Digite o valor que o node vai armazenar: ");
-      scanf("%d", &data);
-      printf("Digite a posição do node.");
-      scanf("%d", &position);
-      insertAtPosition(&head, data, position);
-      break;
-    case 6:
-      printf("Digite a posição do node: ");
-      scanf("%d", &position);
-      deleteAtPosition(&head, position);
-      break;
-    case 7:
-      printListForward(head);
-      break;
-    case 8:
-      printListReverse(head);
-      break;
-    case 9:
-      bubbleSortCrescente(head);
-      break;
-    case 10:
-      bubbleSortDecrescente(head);
-      break;
-    case 11:
-      fill(head);
-      break;
-    case 0:
-      SUCESS("Encerrando...");
-      freeAll(head);
-      break;
-    default:
-      ERROR("Opção não reconhecida.");
-      break;
-    }
-  } while (option != 0);
-}
-
-static void usage() {
-  printf("Usage: %s\n", PROG_NAME);
-  puts("Comando\t Opção\t Descrição");
-  puts("pop\t\t remover o último node da lista");
-  puts("push\t\t adicionar um node ao final da lista");
-  puts("shift\t\t remover primeiro node da lista");
-  puts("unshift\t\t adicionar um node ao inicio da lista");
-  puts("delete\t[n]\t remover node na posição n");
-  puts("insert");
-  puts("print");
-  puts("sort");
-  puts("fill\t\t preencher a lista com números aleatórios");
-  puts("exit\t\t encerrar programa");
-}
-
-void cmd_pop(Node **head) { pop(head); }
-
-void cmd_shift(Node **head) { shift(head); }
-
-void cmd_print(Node **head) { printListForward(*head); }
-
-void cmd_sort(Node **head) { bubbleSortCrescente(*head); }
-
-void cmd_fill(Node **head) { fill(*head); }
-
-void cmd_exit(Node **head) {
-  SUCESS("Encerrando...");
-  freeAll(*head);
-  exit(EXIT_SUCCESS);
-}
-
-void cmd_help(Node **head) { usage(); }
-
-/* Command commands[] = {
-    {"help", cmd_help},   {"exit", cmd_exit}, {"pop", cmd_pop},
-    {"shift", cmd_shift}, {"fill", cmd_fill}, {"sort", cmd_sort},
-    {"print", cmd_print},
-}; */
-/*
-int execute_command(const char *input, Node **head) {
-  size_t count = sizeof(commands) / sizeof(commands[0]);
-
-  for (size_t i = 0; i < count; i++) {
-    if ((strcmp(input, commands[i].name)) == 0) {
-      commands[i].fn(head);
-      return 1;
-    }
-  }
-  return 0;
-} */
 
 static int help(char *arg) {
   printf("Usage of program\n");
   return 0;
 }
 
-int main(int argc, char *argv[]) {
+Command commands[] = {
+    {"help", help, "See this message"},
+    {"quit"},
+    {"pop"},
+    {"push"},
+    {"shift"},
+    {"unshift"},
+    {"fill"},
+    {"delete"},
+    {"print"},
+    {"sort"},
+    {NULL, NULL, NULL},
+};
 
-  printf("%s\n", argv[0]);
-  Command commands[] = {
-      {"help", help, "See this message"},
-      {"quit"},
-      {"pop"},
-      {"push"},
-      {"shift"},
-      {"unshift"},
-      {"fill"},
-      {"delete"},
-      {"print"},
-      {"sort"},
-      {NULL, NULL, NULL},
-  };
+int main(int argc, char *argv[]) {
 
   initialize_readline(argv[0], commands);
 
